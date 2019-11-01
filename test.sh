4937180def0a40b36172adeeafd9024e718a661b
bsteve456 <stevealan1@protonmail.com>
2019-11-01 16:55:23 +0100

    add printf and ft_printf case and test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+gcc -Wall -Werror -Wextra printf_case.c -o true_printf
+./true_printf > expected
+gcc -Wall -Werror -Wextra ft_printf_case.c ../ft_printf.c -o my_printf
+./my_printf > output
+DIFF=$(diff expected output)
+if [ "$DIFF" != "" ]
+	then
+		echo "KO"
+		#exit
+	else
+		echo "OK"
+	fi
+diff -u expected output > diff_file
+rm my_printf true_printf output
