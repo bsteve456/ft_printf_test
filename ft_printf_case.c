9c389c2aeac3826cb49d9a91dc5f12deb9e200a8
Steve-alan BALEBA BALEBA <stbaleba@e3r13p2.42.fr>
2019-11-14 14:52:00 +0100

    check error

--- ft_printf_case.c
+++ ft_printf_case.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 16:11:50 by blacking          #+#    #+#             */
-/*   Updated: 2019/11/14 11:43:30 by blacking         ###   ########.fr       */
+/*   Updated: 2019/11/14 14:46:43 by stbaleba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,17 @@ int main()
 	ft_printf("%*.*d\n", 10, 4, 5);
 	ft_printf("%10.4s\n", "abc");
 	ft_printf("%10.2s\n", "abc");
-	ft_printf("%010.4s\n", "abc");
-	ft_printf("%010.2s\n", "abc");
 	ft_printf("%10.*s\n", 5,"abc");
 	ft_printf("%10.*s\n", -5,"abc");
 	ft_printf("%10.*d\n", -5, 20);
-	ft_printf("%010.*s\n", -5,"abc");
 	ft_printf("%010.*d\n", -5, 10);
-	ft_printf("%010s\n", "abc");
 	ft_printf("%-10.*s\n", -5,"abc");
-	ft_printf("%-010.*s\n", -5,"abc");
 	ft_printf("%-010.*d\n", -5, 10);
-	ft_printf("%-010.*s\n", 5,"abc");
 	ft_printf("%-010.*d\n", 5, 10);
-
+	ft_printf("%010.5d\n", 2500);
+	ft_printf("%10.2d\n", -2500);
+	ft_printf("%10.3d\n", -2500);
+	ft_printf("%10.4d\n", -2500);
+	ft_printf("%10.5d\n", -2500);
+	ft_printf("%-*d", -5, 6);
 }
--- printf_case.c
+++ printf_case.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 16:11:50 by blacking          #+#    #+#             */
-/*   Updated: 2019/11/14 11:43:12 by blacking         ###   ########.fr       */
+/*   Updated: 2019/11/14 14:46:52 by stbaleba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,17 @@ int main()
 	printf("%*.*d\n", 10, 4, 5);
 	printf("%10.4s\n", "abc");
 	printf("%10.2s\n", "abc");
-	printf("%010.4s\n", "abc");
-	printf("%010.2s\n", "abc");
 	printf("%10.*s\n", 5,"abc");
 	printf("%10.*s\n", -5,"abc");
 	printf("%10.*d\n", -5, 20);
-	printf("%010.*s\n", -5,"abc");
 	printf("%010.*d\n", -5, 10);
-	printf("%010s\n", "abc");
 	printf("%-10.*s\n", -5,"abc");
-	printf("%-010.*s\n", -5,"abc");
 	printf("%-010.*d\n", -5, 10);
-	printf("%-010.*s\n", 5,"abc");
 	printf("%-010.*d\n", 5, 10);
-
+	printf("%010.5d\n", 2500);
+	printf("%10.2d\n", -2500);
+	printf("%10.3d\n", -2500);
+	printf("%10.4d\n", -2500);
+	printf("%10.5d\n", -2500);
+	printf("%-*d", -5, 6);
 }
